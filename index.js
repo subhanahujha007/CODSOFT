344a59795f59cefea70da97589d3f4b845c49ed8
subhanahujha007 <138554397+subhanahujha007@users.noreply.github.com>
2023-09-01 13:29:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let buttons=document.querySelectorAll('.button');
+let string="";
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML =='='){
+        string=eval(string);
+document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML =='C'){
+        string='';
+document.querySelector('input').value=string;
+    } else if(e.target.innerHTML =='M+'){
+        if(string<0){string=string*(-1)};
+document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML =='M-'){
+        string=string*(-1);
+document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML=='%'){
+       string=string/100;
+       document.querySelector('input').value=string;
+    }
+   
+    else{
+  
+console.log(e.target);
+string=string+e.target.innerHTML;
+document.querySelector('input').value=string;
+    }})
+})
